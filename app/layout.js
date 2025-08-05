e06036978958e3aa09bcb864254aa19f63e26a90
flores8 <flores.lauralee@gmail.com>
2025-08-05 10:43:23 -0700

    update favicon

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
